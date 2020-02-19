731db7cc366315d65d00f1adbe36b3f2e57ae43c
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-19 17:52:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,4 +6,10 @@ class App < Sinatra::Base
     @user_name = params[:name].reverse
     "#{@user_name}"
   end
+  
+ get '/square/:number' do 
+   @number = params[:number].to_i**2 
+   "#{@number}"
+ end 
+ 
 end
